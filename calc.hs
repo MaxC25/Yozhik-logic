f042fca21ad73cfc7092ed5797059e17c51bbd88
MaxC25 <7951@balcarras.gloucs.sch.uk>
2026-02-23 09:51:49 +0000

    Still work in progress

--- calc.hs
+++ calc.hs
@@ -1,10 +1,32 @@
---import Control.Monad
---import Data.Ref
---import Data.IORef
-import Numeric.Natural
-import Data.Ratio
+import Data.Ratio (Ratio)
+import Control.Monad
+import Data.Ref
+import Data.IORef
+--import Data.Ratio
 import Data.Bits
-import Data.Word (Word8)
+import Data.Char
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Char
+import Text.Parsec.Expr
+
+data P = IMPLY P P
+  |NIMPLY P P
+  |OR P P
+  |NOR P P
+  |XOR P P
+  |XNOR P P
+  |AND P P
+  |NAND P P
+  |NOT P
+p :: Parser Integer
+p = do
+    n <- many1 digit
+    return (read n)
+
+
+
+--import Data.Word (Word8)
 {-qsort :: IOUArray Int Int -> Int -> Int -> IO () -- Thank you Dan Burton https://stackoverflow.com/questions/7717691/why-is-the-minimalist-example-haskell-quicksort-not-a-true-quicksort
 qsort a lo hi = do
   (h,l,p,t) <- liftM4 (,,,) z z z z
@@ -35,9 +57,11 @@ qsort a lo hi = do
     qsort a lo hi'
     lo' <- fmap succ (get l)
     qsort a lo' hi --my code
--}
-fallfact :: Integral  -> Integral -> Integral
-fallfact _ 0 = 0
+
+fallfact :: (Integral x, Integral n) => x -> n -> Integer --Falling factorial
+fallfact _ 1 = 1
 fallfact x n =
-  if n<=1 then 1 else x*fallfact(x,n-1)
-main = print(fallfact(4,2))
+  if n<0 then 1 else toInteger(x)*(fallfact x (n-1))
+kmap :: ()
+-}
+main = print(p)
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 This is a work in progress, this does not work yet. Still if you try to compile it, please use -XDatatypeContexts.
+This is a logic program
